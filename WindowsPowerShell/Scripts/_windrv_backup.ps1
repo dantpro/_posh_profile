188df7f42befc6066837ff088c5bb97869fa498e
dantpro <dant@dant.pro>
2025-02-08 10:50:33 +0700

    something was changed

--- WindowsPowerShell/Scripts/_windrv_backup.ps1
+++ WindowsPowerShell/Scripts/_windrv_backup.ps1
@@ -1,15 +1,15 @@
 # https://winitpro.ru/index.php/2014/07/31/eksport-drajverov-s-pomoshhyu-powershell-v-windows-8-1-u1/
 
-Export-WindowsDriver –Online -Destination C:\-\drv\_exp\
-# & "dism /online /export-driver /destination:C:\-\drv\_exp"
+Export-WindowsDriver –Online -Destination C:\-\drv\
+# & "dism /online /export-driver /destination:C:\-\drv"
 
-# & "pnputil.exe /add-driver C:\-\drv\_exp\*.inf /subdirs /install"
+# & "pnputil.exe /add-driver C:\-\drv\*.inf /subdirs /install"
 
-# $BackupDrv = Export-WindowsDriver -Online -Destination C:\-\drv\_exp\
+# $BackupDrv = Export-WindowsDriver -Online -Destination C:\-\drv\
 # $BackupDrv | Select-Object ClassName, ProviderName, Date, Version | Sort-Object ClassName
 
 # $BackupDrv| Select-Object ClassName, ProviderName, Date, Version |
-#    Export-Csv C:\-\drv\_exp\drivers_list.csv -NoTypeInformation -Encoding UTF8
+#    Export-Csv C:\-\drv\drivers_list.csv -NoTypeInformation -Encoding UTF8
 
 # pnputil.exe /enum-drivers
 # pnputil.exe /add-driver C:\#\DRV\_EXP\*.inf /subdirs /install
