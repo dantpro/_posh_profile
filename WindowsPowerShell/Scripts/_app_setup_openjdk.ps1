c0198ff118472dd44e1877dd25a17a787e81165c
Dmitry Antonov <dant@dant.pro>
2025-06-17 10:07:27 +0700

    something was changed

---
+++ WindowsPowerShell/Scripts/_app_setup_openjdk.ps1
@@ -0,0 +1,6 @@
+# https://jdk.java.net/java-se-ri/11-MR3\
+
+#<#
+[Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Programs\jdk-11.0.0.2", "Machine")
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + "%JAVA_HOME%", "Machine")
+#>
