ed7fab1a96a0b49c0b45a57b1908a8e8c97e135c
Dmitry Antonov <dant@dant.pro>
2025-07-09 17:47:05 +0700

    something was changed

---
+++ WindowsPowerShell/Scripts/_ads_krd_get_enctype_all.ps1
@@ -0,0 +1,48 @@
+#
+# https://4sysops.com/archives/find-active-directory-accounts-configured-for-des-and-rc4-kerberos-encryption/
+# 
+$encTypes = @("Not defined - defaults to RC4_HMAC_MD5",`
+    "DES_CBC_CRC",`
+    "DES_CBC_MD5",`
+    "DES_CBC_CRC | DES_CBC_MD5",`
+    "RC4",`
+    "DES_CBC_CRC | RC4",`
+    "DES_CBC_MD5 | RC4",`
+    "DES_CBC_CRC | DES_CBC_MD5 | RC4",`
+    "AES 128",`
+    "DES_CBC_CRC | AES 128",`
+    "DES_CBC_MD5 | AES 128",`
+    "DES_CBC_CRC | DES_CBC_MD5 | AES 128",`
+    "RC4 | AES 128",`
+    "DES_CBC_CRC | RC4 | AES 128",`
+    "DES_CBC_MD5 | RC4 | AES 128",`
+    "DES_CBC_CBC | DES_CBC_MD5 | RC4 | AES 128",`
+    "AES 256",`
+    "DES_CBC_CRC | AES 256",`
+    "DES_CBC_MD5 | AES 256",`
+    "DES_CBC_CRC | DES_CBC_MD5 | AES 256",`
+    "RC4 | AES 256",`
+    "DES_CBC_CRC | RC4 | AES 256",`
+    "DES_CBC_MD5 | RC4 | AES 256",`
+    "DES_CBC_CRC | DES_CBC_MD5 | RC4 | AES 256",`
+    "AES 128 | AES 256",`
+    "DES_CBC_CRC | AES 128 | AES 256",`
+    "DES_CBC_MD5 | AES 128 | AES 256",`
+    "DES_CBC_MD5 | DES_CBC_MD5 | AES 128 | AES 256",`
+    "RC4 | AES 128 | AES 256",`
+    "DES_CBC_CRC | RC4 | AES 128 | AES 256",`
+    "DES_CBC_MD5 | RC4 | AES 128 | AES 256",`
+    "DES+A1:C33_CBC_MD5 | DES_CBC_MD5 | RC4 | AES 128 | AES 256")
+
+$EncVal = Get-ADUser -SearchBase "DC=contoso,DC=dom" `
+    -Filter * -properties msDS-SupportedEncryptionTypes
+
+foreach($e in $EncVal){
+    try {
+        $e.Name + "," + $encTypes[$e.'msDS-SupportedEncryptionTypes']
+    }
+    catch{
+        $e.Name + "," + $encTypes[0]
+    }    
+}
+
