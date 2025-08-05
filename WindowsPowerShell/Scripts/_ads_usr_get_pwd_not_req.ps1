989d5a3d2514072dffb129a23315db81f0916825
Dmitry Antonov <dant@dant.pro>
2025-08-05 11:49:20 +0700

    something was changed

---
+++ WindowsPowerShell/Scripts/_ads_usr_get_pwd_not_req.ps1
@@ -0,0 +1,9 @@
+Get-ADUser -Filter {Enabled -eq $true} -Properties userAccountControl |
+    Where-Object { ($_.userAccountControl -band 0x20) } |
+    Select-Object Name, userAccountControl, distinguishedName
+
+"---"
+
+Get-ADUser -Filter {PasswordNotRequired -eq $true} -Properties PasswordNotRequired |
+    Select-Object Name, PasswordNotRequired, distinguishedName
+
